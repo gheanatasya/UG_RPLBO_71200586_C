7a16942537b711d08520c73c4f0ad809d6c807b4
gheanatasya <78341297+gheanatasya@users.noreply.github.com>
2022-02-16 17:57:59 +0700

    Add files via upload

---
+++ hitungDiskon.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class hitungDiskon {
+    Scanner inp = new Scanner(System.in);
+    System.out.println("Banyak belanjaan: ");
+    int jmlBarang = inp.nextInt();
+    for (int jmlBarang = 0; jmlBarang<ranges.length )
+
+}
